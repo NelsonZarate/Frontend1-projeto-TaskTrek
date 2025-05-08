8f4104393ae3cf21c9d99458b3b8787ea723a061
Nelson Zarate <nelsonpinto1415@gmail.com>
2025-05-09 00:57:16 +0100

    add:Web componet

--- src.js
+++ src.js
@@ -3,6 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   setInterval(displayCurrentTime, 1000);
   // Initial call to display the time immediately
   displayCurrentTime();
+  class CustomFooter extends HTMLElement {
+    year = getYear();
+    connectedCallback() {
+      // Insert HTML content for the menu
+      this.innerHTML = ` <footer class="text-center py-4 bg-light mt-5 border-top">
+        <p class="mb-0">&copy ${this.year} TaskTrek. All rights reserved.</p>
+        </footer>
+  `;
+    }
+  }
+
+  // Register the custom element with the tag 'my-custom-menu'
+  customElements.define('my-custom-footer', CustomFooter);
 });
 
 function displayCurrentTime() {
@@ -13,4 +26,12 @@ function displayCurrentTime() {
   if (timeElement) {
     timeElement.textContent = formattedTime;
   }
+}
+
+function getYear() {
+  // Get the current year
+  const date = new Date();
+  const year = date.getFullYear();
+  // Display the current year in the 
+  return year;
 }
